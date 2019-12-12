b8d96ee9cda22583e1c7db2166307477f6083e22
jaywongkot <onzonstudios@gmail.com>
2019-12-12 16:05:18 -0400

    Added form to create new post

--- jmportfolio/blog/views.py
+++ jmportfolio/blog/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
 def post_list(request):
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') # add to published_date to display newest post first. 
-  return render(request, '../templates/post_list.html', { 'blogs':posts })
+  return render(request, '../templates/post_list.html', { 'blogs': posts }) # string in '' use to loop through post in post_list.html
+
+def post_detail(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  return render(request, '../templates/post_detail.html', {'post': post}) # string in '' use to check condition in post_detail.html
+
+def post_new(request):
+  form = PostForm()
+  return render(request, '../templates/post_edit.html', {'form': form}) # string in '' use to display form in post_edit.html
