df0a702be0eb702d92a6197c8df2c4b19be16a71
Jay Wongkot <onzonstudios@gmail.com>
2020-04-15 11:36:26 -0300

    Update blog app

--- jmportfolio/jmportfolio/settings.py
+++ jmportfolio/jmportfolio/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
     'core.apps.CoreConfig',
     'blog.apps.BlogConfig',
 ]
